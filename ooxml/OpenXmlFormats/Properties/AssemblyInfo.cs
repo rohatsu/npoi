67756a9105d440780a9480b3234d6f3f2ca7b7e0
rohatsu <rohatsu@users.noreply.github.com>
2019-04-08 16:43:37 +0900

    untrusted caller fix

--- ooxml/OpenXmlFormats/Properties/AssemblyInfo.cs
+++ ooxml/OpenXmlFormats/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Security;
 [assembly: AssemblyVersion("2.2.1.0")]
 [assembly: AssemblyFileVersion("2.2.1.0")]
 [assembly: AssemblyInformationalVersion("2.0.0.0")]
-[assembly: AllowPartiallyTrustedCallers]
+//[assembly: AllowPartiallyTrustedCallers]
